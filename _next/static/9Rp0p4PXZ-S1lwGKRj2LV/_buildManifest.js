f7c6735c204632a2b8d1b77d3fa5686d251f9e26
Erik Söderberg <erikvsoderberg@gmail.com>
2020-09-07 20:02:57 +0200

    remove everything

--- _next/static/9Rp0p4PXZ-S1lwGKRj2LV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002F9727125669b3604bb4a1.css","static\u002Fchunks\u002Fpages\u002Findex-33cd19278d5e4c36651a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
